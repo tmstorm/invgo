e758668c18e537088a7dd7fec1357d3120fd6f0d
Dillon Storm <dstorm@bayshoresystems.com>
2025-12-05 12:21:37 -0800

    chore: eliminate endpoint boilerplate with NewPublicMethod

--- endpoint_methods.go
+++ endpoint_methods.go
@@ -1,9 +1,45 @@
 package invgo
 
+/*
+All endpoints must be implemented here to be available in the Invgo public API
+
+To implement a new endpoint add its methods to invgo/endpoints/endpoint_name.go.
+Then add it as a method to the Invgo Client using NewPublicMethod.
+
+Example:
+	func (c *Client) BreakingNews() *endpoints.BreakingNewsMethods {
+		return NewPublicMethod[endpoints.BreakingNewsMethods](c, "/breakingnews")
+	}
+*/
+
 import (
+	"unsafe"
+
 	"github.com/tmstorm/invgo/endpoints"
 	"github.com/tmstorm/invgo/internal/methods"
 )
+
+// NewPublicMethod should be used when adding a new enpoint to the Invgo public API
+// T must be a struct whose first field is methods.MethodCall
+func NewPublicMethod[T any](c *Client, endpoint string) *T {
+	var zero T
+	result := &zero
+
+	ep := c.APIURL.JoinPath(endpoint)
+
+	mcPtr := (*methods.MethodCall)(unsafe.Pointer(result))
+
+	*mcPtr = methods.MethodCall{
+		Client: &methods.Client{
+			HTTPClient:    c.HTTPClient,
+			CurrentScopes: c.CurrentScopes,
+			APIURL:        c.APIURL,
+		},
+		Endpoint: ep,
+	}
+
+	return result
+}
 
 // BreakingNews manages the /breakingnews endpoint
 // Get: Returns the requested Breaking News
@@ -11,33 +47,13 @@ import (
 // Put: Modifies a set of Breaking News
 // See https://releases.invgate.com/service-desk/api/#breakingnews
 func (c *Client) BreakingNews() *endpoints.BreakingNewsMethods {
-	ep := c.APIURL.JoinPath("/breakingnews")
-	return &endpoints.BreakingNewsMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.BreakingNewsMethods](c, "/breakingnews")
 }
 
 // BreakingNewsAll gets all the Breaking News.
 // See https://releases.invgate.com/service-desk/api/#breakingnewsall
 func (c *Client) BreakingNewsAll() *endpoints.BreakingNewsAllMethods {
-	ep := c.APIURL.JoinPath("/breakingnews.all")
-	return &endpoints.BreakingNewsAllMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.BreakingNewsAllMethods](c, "/breakingnews.all")
 }
 
 // BreakingNewsStatus manages the updates of the breaking news.
@@ -45,83 +61,32 @@ func (c *Client) BreakingNewsAll() *endpoints.BreakingNewsAllMethods {
 // Post: Creates a new update to the given Breaking News
 // See https://releases.invgate.com/service-desk/api/#breakingnewsstatus
 func (c *Client) BreakingNewsStatus() *endpoints.BreakingNewsStatusMethods {
-	ep := c.APIURL.JoinPath("/breakingnews.status")
-	return &endpoints.BreakingNewsStatusMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.BreakingNewsStatusMethods](c, "/breakingnews.status")
 }
 
 // BreakingNewsAttributesStatus gets all the possible status for the Breaking News'
 // and their descriptions.
 // See https://releases.invgate.com/service-desk/api/#breakingnewsattributesstatus
 func (c *Client) BreakingNewsAttributesStatus() *endpoints.AttributesMethods {
-	ep := c.APIURL.JoinPath("/breakingnews.attributes.status")
-
-	return &endpoints.AttributesMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.AttributesMethods](c, "/breakingnews.attributes.status")
 }
 
 // BreakingNewsAttributesType gets all the importance types of the Breaking News.
 // See https://releases.invgate.com/service-desk/api/#breakingnewsattributestype
 func (c *Client) BreakingNewsAttributesType() *endpoints.AttributesMethods {
-	ep := c.APIURL.JoinPath("/breakingnews.attributes.type")
-	return &endpoints.AttributesMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.AttributesMethods](c, "/breakingnews.attributes.type")
 }
 
 // HelpDesks manages the help desks
 // See https://releases.invgate.com/service-desk/api/#helpdesks
 func (c *Client) HelpDesks() *endpoints.HelpDesksMethods {
-	ep := c.APIURL.JoinPath("/helpdesks")
-	return &endpoints.HelpDesksMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.HelpDesksMethods](c, "/helpdesks")
 }
 
 // Categories is used to get all categories for the current Invgate instance
 // See https://releases.invgate.com/service-desk/api/#categories
 func (c *Client) Categories() *endpoints.CategoriesMethods {
-	ep := c.APIURL.JoinPath("/categories")
-	return &endpoints.CategoriesMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.CategoriesMethods](c, "/categories")
 }
 
 // Incident manages the /incident endpoint
@@ -130,95 +95,35 @@ func (c *Client) Categories() *endpoints.CategoriesMethods {
 // Put: Change attributes of a request
 // See https://releases.invgate.com/service-desk/api/#incident
 func (c *Client) Incident() *endpoints.IncidentMethods {
-	ep := c.APIURL.JoinPath("/incident")
-	return &endpoints.IncidentMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.IncidentMethods](c, "/incident")
 }
 
 // Incidents is used to get Incidents from the Invgate API
 // See https://releases.invgate.com/service-desk/api/#incidents
 func (c *Client) Incidents() *endpoints.IncidentsMethods {
-	ep := c.APIURL.JoinPath("/incidents")
-	return &endpoints.IncidentsMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.IncidentsMethods](c, "/incidents")
 }
 
 // IncidentsByStatus gets incidents by the given set of status IDs
 // See https://releases.invgate.com/service-desk/api/#incidentsbystatus
 func (c *Client) IncidentsByStatus() *endpoints.IncidentsByStatusMethods {
-	ep := c.APIURL.JoinPath("/incidents.by.status")
-	return &endpoints.IncidentsByStatusMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.IncidentsByStatusMethods](c, "/incidents.by.status")
 }
 
 // IncidentAttributesStatus gets all the status types usable for an incident
 // See https://releases.invgate.com/service-desk/api/#incidentattributestype
 func (c *Client) IncidentAttributesStatus() *endpoints.AttributesMethods {
-	ep := c.APIURL.JoinPath("/incident.attributes.status")
-	return &endpoints.AttributesMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.AttributesMethods](c, "/incident.attributes.status")
 }
 
 // IncidentAttributesType gets all the types usable for an incident
 // See https://releases.invgate.com/service-desk/api/#incidentattributestype
 func (c *Client) IncidentAttributesType() *endpoints.AttributesMethods {
-	ep := c.APIURL.JoinPath("/incident.attributes.type")
-	return &endpoints.AttributesMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.AttributesMethods](c, "/incident.attributes.type")
 }
 
 // ServiceDeskVersion returns the current version of the Service Desk instance
 // See https://releases.invgate.com/service-desk/api/#sdversion
 func (c *Client) ServiceDeskVersion() *endpoints.ServiceDeskVersionMethods {
-	ep := c.APIURL.JoinPath("/sd.version")
-	return &endpoints.ServiceDeskVersionMethods{
-		MethodCall: methods.MethodCall{
-			Client: &methods.Client{
-				HTTPClient:    c.HTTPClient,
-				CurrentScopes: c.CurrentScopes,
-				APIURL:        c.APIURL,
-			},
-			Endpoint: ep,
-		},
-	}
+	return NewPublicMethod[endpoints.ServiceDeskVersionMethods](c, "/sd.version")
 }
--- endpoints/doc.go
+++ endpoints/doc.go
@@ -3,6 +3,7 @@ Package endpoints is the implementation of Invgate endpoints and their methods.
 
 Each endpoint should be in its own file for clarity and readbility e.g. breaking news should be BreakingNews.go.
 To expose the endpoint through the Invgo public API a Client method should be defined for each it in invgo/endpoint_methods.go.
+
 If a new endpoint is added ensure it is added to invgo/coverage.go along with its related methods. This ensures the coverage script catches that
 it has been implemented.
 */
